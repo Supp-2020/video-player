24ba9a0ef3bcdbfbe55174dc444ea1c507d410ca
Supp-2020 <supreetpradhan@gmail.com>
2025-04-07 00:24:45 +0530

    Used react-player package with youtube urls

--- src/components/MainBody/VideoPlayer/VideoPlayer.jsx
+++ src/components/MainBody/VideoPlayer/VideoPlayer.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import djVideo from '../../../video/dj-video.mp4'
 import './VideoPlayer.css'
+import ReactPlayer from 'react-player'
 
 const VideoPlayer = () => {
   return (
     <div className='video-player-main-wrapper' data-testid="video-player-wrapper">
       <div className='video-player-container'>
         <div className='video-player-video'>
-          <video width={300} autoPlay={true} controls>
+{/*           <video width={800} height={300}autoPlay={true} controls>
             <source src={djVideo} type='video/mp4'/>
             Your Browser does not support video
-          </video>
+          </video> */}
+          <ReactPlayer url={'https://www.youtube.com/watch?v=Cp-1t9B62zc'}/>
         </div>
       </div>
     </div>
